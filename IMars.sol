a04e6398f99bb85f1be1bf2a8bc4abf394961862
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-12-08 16:18:11 +0530

    Update IMars.sol

--- IMars.sol
+++ IMars.sol
@@ -6,12 +6,9 @@ pragma experimental ABIEncoderV2;
 interface IMars {
 
     function deposit(uint256 _pid, uint256 _wantAmt) external;
-
     function withdraw(uint256 _pid, uint256 _wantAmt) external;
-
     function withdrawAll(uint256 _pid) external;
-
     function claim() external;
-
+    function emergencyWithdraw(uint256 _pid) external;
     function getVestingAmount() external view returns (uint256 _lockAmount, uint256 _claimableAmount);
 }
