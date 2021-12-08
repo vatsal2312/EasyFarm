5ebc7a6458d840c6283a184121063e037b40a9ab
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-12-08 15:16:54 +0530

    Update EasyFarmCore.sol

--- EasyFarmCore.sol
+++ EasyFarmCore.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./IERC20.sol";
+import "./Address.sol";
+import "./SafeERC20.sol";
+import "./SafeMath.sol";
+import "./Ownable.sol";
+import "./ReentrancyGuard.sol";
 import "./EasyFarmToken.sol";
 import "./interface/IEasyFarmStrategy.sol";
 
