0609a58d1b3b9d7144c0b2bf80b6b5462c4bdd4c
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-12-08 15:20:21 +0530

    Update StrategyForMars.sol

--- StrategyForMars.sol
+++ StrategyForMars.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: GPL-v3
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "../EasyFarmToken.sol";
+import "./IERC20.sol";
+import "./Address.sol";
+import "./SafeERC20.sol";
+import "./SafeMath.sol";
+import "./Ownable.sol";
+import "./EasyFarmToken.sol";
 
-import "../interface/IEasyFarmStrategy.sol";
-import "../interface/IMars.sol";
-import "../interface/ISwapRouter.sol";
+import "./IEasyFarmStrategy.sol";
+import "./IMars.sol";
+import "./ISwapRouter.sol";
 
 contract StrategyForMars is Ownable, IEasyFarmStrategy {
     using SafeMath for uint256;
